4ae0140a2028d6359abbc6904819f5ec3da557d5
Sameer-aces <mohammed.sameer@aces-co.com>
2023-05-29 21:33:40 +0530

    server start comment

--- ocean_backend/views.py
+++ ocean_backend/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 def start(request):
-    return HttpResponse('<h1> Server Started </h1>')
+    return HttpResponse('<h1> "Server Started"</h1>')
