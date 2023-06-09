19ca8e23b0a97714ef070812d3add574ab8642d9
Sameer-aces <mohammed.sameer@aces-co.com>
2023-06-09 10:24:35 +0530

    ..

--- ML_algo/ML_model/ML_model.py
+++ ML_algo/ML_model/ML_model.py
@@ -1,4 +1,18 @@
-import pickle 
+# import pickle 
+
+# def get_model():
+#     """imports ML model pickle file and then returns that model instance
+
+#     Returns:
+#         Machine Learning Model: Desicion Tree Algorithm model
+#     """    
+#     # import model form the pickle file of model using pickle library 
+#     model = pickle.load(open('ML_algo\ML_model\model.pkl', 'rb'))
+    
+#     return model
+import os
+import pickle
+from django.conf import settings
 
 def get_model():
     """imports ML model pickle file and then returns that model instance
@@ -6,7 +20,10 @@ def get_model():
     Returns:
         Machine Learning Model: Desicion Tree Algorithm model
     """    
-    # import model form the pickle file of model using pickle library 
-    model = pickle.load(open('ML_algo\ML_model\model.pkl', 'rb'))
-    
+    # Construct the full path to the pickle file.
+    model_path = os.path.join(settings.BASE_DIR, 'ML_algo', 'ML_model', 'model.pkl')
+
+    # Import model form the pickle file of model using pickle library 
+    model = pickle.load(open(model_path, 'rb'))
+
     return model
